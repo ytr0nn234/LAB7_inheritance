a9e79183ca4af8eb996adb146340915380b39db4
ytr0nn234 <g00467242@atu.ie>
2025-12-07 23:25:01 +0000

    Created Person class

---
+++ src/main/java/Person.java
@@ -0,0 +1,46 @@
+public class Person {
+    private String name;
+    private String address;
+    private String phoneNumber;
+
+    public Person() {
+        this.name = "";
+        this.address = "";
+        this.phoneNumber = "";
+    }
+
+    public Person(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{name='" + name + "', address='" + address + "', phoneNumber='" + phoneNumber + "'}";
+    }
+}
